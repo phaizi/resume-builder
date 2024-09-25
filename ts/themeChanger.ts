f60c9360a2b78c43509587c22adf86e5f9d1b3f5
phaizi <faizan.mansur87@gmail.com>
2024-09-25 06:26:47 +0500

    Restructuring javascript and typescript files

--- ts/themeChanger.ts
+++ ts/themeChanger.ts
@@ -1,7 +1,8 @@
-"use strict";
+export { themeChanger, applyTheme };
 
-const themeChanger = (sourceID: string, targetID: string): void => {
-  const source: HTMLElement = document.querySelector(sourceID)!;
+function themeChanger(sourceID: string): void {
+  const targetID: string = "#applied-box";
+  const source: HTMLElement = document.querySelector(`#${sourceID}`)!;
   const target: HTMLElement = document.querySelector(targetID)!;
 
   // getting the positions of elements
@@ -23,97 +24,99 @@ const themeChanger = (sourceID: string, targetID: string): void => {
   // timeout is set to make theme changes to take effect after the animation
   setTimeout(() => {
     source.style.transition = "left 0s";
-    const header: HTMLElement = document.querySelector("#layout-header")!;
-    const appliedBox: HTMLElement = document.querySelector("#applied-box")!;
-    const aside: HTMLElement = document.querySelector("#layout-aside")!;
-    const main: HTMLElement = document.querySelector("#layout-main")!;
 
-    const photo: HTMLImageElement = document.querySelector("#photo")!;
-    const locationImg: HTMLImageElement =
-      document.querySelector("#location-img")!;
-    const telImg: HTMLImageElement = document.querySelector("#tel-img")!;
-    const emailImg: HTMLImageElement = document.querySelector("#email-img")!;
-    const linkedinImg: HTMLImageElement =
-      document.querySelector("#linkedin-img")!;
-    const gitImg: HTMLImageElement = document.querySelector("#git-img")!;
+    // getting new theme
+    const theme: string = sourceID.slice(4);
 
-    const sectionHeadingsArray: NodeListOf<HTMLElement> =
-      document.querySelectorAll(".section-heading");
-    const headingsArray: NodeListOf<HTMLElement> =
-      document.querySelectorAll(".heading");
-    const bgCirclesArray: NodeListOf<HTMLElement> =
-      document.querySelectorAll(".bgcircle");
-    const smCirclesArray: NodeListOf<HTMLElement> =
-      document.querySelectorAll(".smcircle");
-    const circlesDisabledArray: NodeListOf<HTMLElement> =
-      document.querySelectorAll(".clr-disabled");
+    // setting color-theme in local storage
+    localStorage.setItem("theme", theme);
 
-    // getting theme-colors
-    const root = document.querySelector(":root")!;
-    const rootStyles = getComputedStyle(root);
-    const theme: string = sourceID.slice(5);
-    const themeColor: string = getComputedStyle(source).backgroundColor;
-    const darkThemeColor: string = rootStyles.getPropertyValue(`--${theme}-dk`);
-    const lightThemeColor: string = rootStyles.getPropertyValue(
-      `--${theme}-lt`
-    );
-    const disabledColor: string = rootStyles.getPropertyValue(
-      `--${theme}-disabled`
-    );
-
-    const toggleList = document.querySelectorAll<HTMLInputElement>(
-      '.switch > input[type="checkbox"]'
-    );
-    const toggleMoverList =
-      document.querySelectorAll<HTMLElement>(".toggle-mover");
-    toggleMoverList.forEach((toggleMover, index) => {
-      const toggle = toggleList.item(index);
-      const isToggleChecked: Boolean = toggle.checked;
-      toggleMover.style.backgroundColor = isToggleChecked
-        ? themeColor
-        : disabledColor;
-      toggle.addEventListener("click", () => {
-        if (toggle.checked === true) {
-          toggleMover.style.backgroundColor = themeColor;
-        } else {
-          toggleMover.style.backgroundColor = disabledColor;
-        }
-      });
-    });
-
-    // applying theme color
-    locationImg.src = `static/${theme}/location.png`;
-    telImg.src = `static/${theme}/tel.png`;
-    emailImg.src = `static/${theme}/email.png`;
-    linkedinImg.src = `static/${theme}/linkedin.png`;
-    gitImg.src = `static/${theme}/git.png`;
-    sectionHeadingsArray.forEach((element) => {
-      element.style.backgroundColor = darkThemeColor;
-    });
-    headingsArray.forEach((element) => {
-      element.style.color = darkThemeColor;
-    });
-    bgCirclesArray.forEach((element) => {
-      element.style.backgroundColor = themeColor;
-      element.style.borderColor = lightThemeColor;
-    });
-    smCirclesArray.forEach((element) => {
-      element.style.backgroundColor = darkThemeColor;
-    });
-    circlesDisabledArray.forEach((element) => {
-      element.style.backgroundColor = disabledColor;
-    });
-    photo.style.filter = `drop-shadow(0px 0px 4px ${darkThemeColor})`;
-    header.style.background = `linear-gradient(100deg, ${darkThemeColor} -50%, ${themeColor})`;
-    // header.style.color = "white";
-    appliedBox.style.backgroundColor = themeColor;
-    aside.style.background = `linear-gradient(100deg, ${darkThemeColor} -50%, ${themeColor})`;
-    aside.style.color = "white";
-    main.style.borderColor = themeColor;
+    applyTheme(theme);
 
     // reverting source element back to its original position
     source.style.width = "30px";
     source.style.left = "0px";
     source.style.zIndex = "0";
   }, 1000);
-};
+}
+
+function applyTheme(theme: string): void {
+  // getting elements for color changes
+  const header: HTMLElement = document.querySelector("#layout-header")!;
+  const appliedBox: HTMLElement = document.querySelector("#applied-box")!;
+  const aside: HTMLElement = document.querySelector("#layout-aside")!;
+  const main: HTMLElement = document.querySelector("#layout-main")!;
+
+  const photo: HTMLImageElement = document.querySelector("#photo")!;
+  const locationImg: HTMLImageElement =
+    document.querySelector("#location-img")!;
+  const telImg: HTMLImageElement = document.querySelector("#tel-img")!;
+  const emailImg: HTMLImageElement = document.querySelector("#email-img")!;
+  const linkedinImg: HTMLImageElement =
+    document.querySelector("#linkedin-img")!;
+  const gitImg: HTMLImageElement = document.querySelector("#git-img")!;
+
+  const sectionHeadingsArray: NodeListOf<HTMLElement> =
+    document.querySelectorAll(".section-heading");
+  const headingsArray: NodeListOf<HTMLElement> =
+    document.querySelectorAll(".heading");
+  const bgCirclesArray: NodeListOf<HTMLElement> =
+    document.querySelectorAll(".bgcircle");
+  const smCirclesArray: NodeListOf<HTMLElement> =
+    document.querySelectorAll(".smcircle");
+  const circlesDisabledArray: NodeListOf<HTMLElement> =
+    document.querySelectorAll(".clr-disabled");
+
+  // getting theme-colors
+  const root = document.querySelector(":root")!;
+  const rootStyles = getComputedStyle(root);
+  const themeColor: string = rootStyles.getPropertyValue(`--${theme}-md`);
+  const darkThemeColor: string = rootStyles.getPropertyValue(`--${theme}-dk`);
+  const lightThemeColor: string = rootStyles.getPropertyValue(`--${theme}-lt`);
+  const disabledColor: string = rootStyles.getPropertyValue(
+    `--${theme}-disabled`
+  );
+
+  const toggleList = document.querySelectorAll<HTMLInputElement>(
+    '.switch > input[type="checkbox"]'
+  );
+  const toggleMoverList =
+    document.querySelectorAll<HTMLElement>(".toggle-mover");
+
+  // applying theme color
+  locationImg.src = `static/${theme}/location.png`;
+  telImg.src = `static/${theme}/tel.png`;
+  emailImg.src = `static/${theme}/email.png`;
+  linkedinImg.src = `static/${theme}/linkedin.png`;
+  gitImg.src = `static/${theme}/git.png`;
+  sectionHeadingsArray.forEach((element) => {
+    element.style.backgroundColor = darkThemeColor;
+  });
+  headingsArray.forEach((element) => {
+    element.style.color = darkThemeColor;
+  });
+  bgCirclesArray.forEach((element) => {
+    element.style.backgroundColor = themeColor;
+    element.style.borderColor = lightThemeColor;
+  });
+  smCirclesArray.forEach((element) => {
+    element.style.backgroundColor = darkThemeColor;
+  });
+  circlesDisabledArray.forEach((element) => {
+    element.style.backgroundColor = disabledColor;
+  });
+  photo.style.filter = `drop-shadow(0px 0px 4px ${darkThemeColor})`;
+  header.style.background = `linear-gradient(100deg, ${darkThemeColor} -50%, ${themeColor})`;
+  appliedBox.style.backgroundColor = themeColor;
+  aside.style.background = `linear-gradient(100deg, ${darkThemeColor} -50%, ${themeColor})`;
+  aside.style.color = "white";
+  main.style.borderColor = themeColor;
+
+  toggleMoverList.forEach((toggleMover, index) => {
+    const toggle = toggleList.item(index);
+    const isToggleChecked: Boolean = toggle.checked;
+    toggleMover.style.backgroundColor = isToggleChecked
+      ? themeColor
+      : disabledColor;
+  });
+}
--- ts/toggleSections.ts
+++ ts/toggleSections.ts
@@ -1,53 +1,48 @@
-const sectionHeadings: string[] = [
-  "experience",
-  "projects",
-  "contact",
-  "skills",
-  "certifications",
-  "education",
-];
+export { toggleDisplaySections };
 
-const addAllListeners = (): void => {
-  const togglesArray: NodeListOf<HTMLInputElement> = document.querySelectorAll(
-    ".switch > input[type='checkbox']"
+function toggleDisplaySections(event: Event, sectionId: string): void {
+  const section: HTMLElement = document.querySelector(
+    `#${sectionId.toLowerCase()}`
+  )!;
+
+  // toggling display
+  section.classList.toggle("hidden");
+
+  const hiddenMainSections: NodeListOf<HTMLElement> = document.querySelectorAll(
+    "#resume-main > section.hidden"
   );
-  // Adding event listeners for toggles
-  togglesArray.forEach((toggle, index) => {
-    toggle.addEventListener("change", () => {
-      const section: HTMLElement = document.querySelector(
-        `#${sectionHeadings[index]}`
-      )!;
+  const hiddenAsideSections: NodeListOf<HTMLElement> =
+    document.querySelectorAll("#resume-aside > section.hidden");
+  const resumeWrapper: HTMLElement = document.querySelector("#resume-wrapper")!;
+  const resumeMain: HTMLElement = document.querySelector("#resume-main")!;
+  const resumeAside: HTMLElement = document.querySelector("#resume-aside")!;
 
-      section.classList.toggle("hidden");
+  // for changing the layout of resume if either columns all sections have toggled to hidden state
+  // and changing the layout back to 2 columns if both columns have atleast 1 section unhidden
+  if (hiddenMainSections.length === 2 || hiddenAsideSections.length === 4) {
+    resumeWrapper.style.gridTemplateColumns = "auto";
+    resumeMain.classList.add("order0");
+    resumeAside.classList.add("order1");
+  } else {
+    resumeWrapper.style.gridTemplateColumns = "min(198px) auto";
+    resumeMain.classList.remove("order0");
+    resumeAside.classList.remove("order1");
+  }
 
-      const hiddenMainSections: NodeListOf<HTMLElement> =
-        document.querySelectorAll("#resume-main > section.hidden");
-      const hiddenAsideSections: NodeListOf<HTMLElement> =
-        document.querySelectorAll("#resume-aside > section.hidden");
-      const resumeWrapper: HTMLElement =
-        document.querySelector("#resume-wrapper")!;
-      const resumeMain: HTMLElement = document.querySelector("#resume-main")!;
-      const resumeAside: HTMLElement = document.querySelector("#resume-aside")!;
+  // Applying color changes on toggle
+  const theme = localStorage.getItem("theme") ?? "orange";
+  const toggle = event.currentTarget as HTMLInputElement;
+  const toggleMover = toggle.parentElement?.lastElementChild as HTMLElement;
+  const isToggleChecked: Boolean = toggle.checked;
 
-      // for changing the layout of resume if either columns all sections have toggled to hidden state
-      // and changing the layout back to 2 columns if both columns have atleast 1 section unhidden
-      if (hiddenMainSections.length === 2 || hiddenAsideSections.length === 4) {
-        resumeWrapper.style.gridTemplateColumns = "auto";
-        resumeMain.classList.add("order0");
-        resumeAside.classList.add("order1");
-      } else {
-        resumeWrapper.style.gridTemplateColumns = "min(198px) auto";
-        resumeMain.classList.remove("order0");
-        resumeAside.classList.remove("order1");
-      }
-    });
-  });
-};
+  const root = document.querySelector(":root")!;
+  const rootStyles = getComputedStyle(root);
+  const themeColor: string = rootStyles.getPropertyValue(`--${theme}-md`);
+  const disabledColor: string = rootStyles.getPropertyValue(
+    `--${theme}-disabled`
+  );
 
-if (document.readyState === "loading") {
-  // Loading hasn't finished yet
-  document.addEventListener("DOMContentLoaded", addAllListeners);
-} else {
-  // `DOMContentLoaded` has already fired
-  addAllListeners();
+  toggleMover.style.backgroundColor = isToggleChecked
+    ? themeColor
+    : disabledColor;
 }
